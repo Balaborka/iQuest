e447846fea460cffe04e32d6922f48c1bf1bc512
Boris Balaban <BorisBalaban71@gmail.com>
2018-12-20 01:06:24 +0300

    Correct input

--- wwwroot/js/game.js
+++ wwwroot/js/game.js
@@ -1,21 +1,50 @@
-function init(data) {
+function init(data) {    
+    initTaskList();
+    hidePopup();
+}
+
+function initTaskList() {
     var container = $('.questions-container');
     var i = 0;
     data.items.forEach(element => {
         i++;
         var row = $('<div />').text("Task " + i).addClass('question');
         row.click(() => {
-            $(".popup").removeClass("hidden");
-            $(".questions-container").addClass("hidden");
-            $(".task-korpus").text("Корпус: " + element.korpus);
-            $(".task-text").text(element.text);
-        });
-        $(".popup-close").click(() => {
-            $(".popup").addClass("hidden");
-            $(".questions-container").removeClass("hidden");
-        });
+            $(".popup").show();
+            $(".questions-container").hide();
+            initPopup(element);
+        });        
         container.append(row);
     });
+}
+
+function initPopup(element) {
+    $(".task-korpus").text("Корпус: " + element.korpus);
+    $(".task-text").text(element.text);
+    $(".check-result").hide();
+
+    $(".btnScan").click(() => {
+        $(".check-result").show();
+    });    
+
+    $(".btnRes").click(() => {       
+        var scanRes = $("#inputRes").val();
+        if (scanRes === element.result) {
+            hidePopup();
+            $(".btnRes").off('click');
+            $('#inputRes').val('');
+        }
+    });
+
+    $(".popup-close").click(() => {
+        hidePopup();
+        $(".check-result").hide();
+    });
+}
+
+function hidePopup() {
+    $(".popup").hide();
+    $(".questions-container").show();
 }
 
 $(function() {
