2860a66a4af7109c0b77dd74799d6d2233e6928c
Boris Balaban <BorisBalaban71@gmail.com>
2019-02-12 14:51:57 +0300

    Edit Registration page List

--- wwwroot/js/registration.js
+++ wwwroot/js/registration.js
@@ -9,16 +9,27 @@ $(function($) {
                 //$(".popup").fadeIn(500);
                 var container = $(".addTeamContainer ol");
                 var liElement = $('<li>').appendTo(container);
+                var btnElement = $('<div>').attr('id', 'btnDeleteTeam').html('x').addClass('btnTeam').appendTo(liElement);
                 var spanElement = $('<span>').addClass('team').text(teamName).appendTo(liElement);
                 var inputElement = $('<input>').addClass('inputTeam')
                 .focusout(() => {
-                    inputElement.hide();
-                    spanElement.show().text(inputElement.val());
+                    if (inputElement.val() != '') {
+                        inputElement.hide();
+                        btnElement.show();
+                        spanElement.show().text(inputElement.val());
+                    }
                 }).appendTo(liElement);
+                btnElement.click(() => {
+                    liElement.remove();
+                    spanElement.remove();
+                    inputElement.remove();
+                    btnElement.remove();
+                });
                 spanElement.click(() => {
+                    btnElement.hide();
                     spanElement.hide();
                     inputElement.show().val(spanElement.text()).focus();
-                })
+                });
             }
         });    
     });
